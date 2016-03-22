b0c0a4d6964bd48f4df89d64677b8a5e44f2bcab
Leszek Grzanka <Leszek.Grzanka@ifj.edu.pl>
2016-03-22 15:34:31 +0100

    refactored

--- usrbinmerge.py
+++ usrbinmerge.py
@@ -4,22 +4,23 @@ from glob import glob
 import pandas
 import numpy as np
 
-def merge(inputfiles, outputfile):
 
-    first_file = pandas.read_csv(inputfiles[0], sep=' ', names=('x','y','z','v'))
+def merge(input_files, output_file):
+    first_file = pandas.read_csv(input_files[0], sep=' ', names=('x', 'y', 'z', 'v'))
     result = first_file
 
-    for file in inputfiles[1:]:
-        data = pandas.read_csv(file, sep=' ', names=('x','y','z','v'))
+    for file in input_files[1:]:
+        data = pandas.read_csv(file, sep=' ', names=('x', 'y', 'z', 'v'))
+        print(np.max(data['x']), np.max(data['y']), np.max(data['z']))
         result['v'] += data['v']
 
-    result['v'] /= len(inputfiles)
+    result['v'] /= len(input_files)
 
-    for axis in ('x','y','z'):
+    for axis in ('x', 'y', 'z'):
         if np.unique(result[axis]).size == 1:
-            result.drop(axis,inplace=True,axis=1)
+            result.drop(axis, inplace=True, axis=1)
 
-    result.to_csv(outputfile, sep=' ', header=False, index=False)
+    result.to_csv(output_file, sep=' ', header=False, index=False)
 
 
 if __name__ == '__main__':
@@ -32,11 +33,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-
-    inputfiles = args.pattern
-
-    print("Input files: ", inputfiles)
+    print("Input files: ", args.pattern)
     print("Output file: ", args.outputfile)
 
-
-    merge(inputfiles=inputfiles, outputfile=args.outputfile)
+    merge(input_files=args.pattern, output_file=args.outputfile)
